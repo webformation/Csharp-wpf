f9d526196c488ad11574e8022747d5e97af3b264
colombani <dc@webformation.fr>
2019-06-13 21:03:47 +0100

    ajout DataGrid simple

---
+++ DatagridSimple/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Datagrid
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DataSet1.PersonDataTable tablePersonne { get; set; }
+        public MainWindow()
+        {
+            tablePersonne = new DataSet1.PersonDataTable();
+            DataSet1TableAdapters.PersonTableAdapter taPersonne = new DataSet1TableAdapters.PersonTableAdapter();
+            taPersonne.Fill(tablePersonne);
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
